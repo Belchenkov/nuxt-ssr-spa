e48503aa061ce5265f6d0864f74f4695ae146c88
Aleksey Belchenkov <u608110@gmail.com>
2020-04-04 13:56:31 +0300

    Creating the Main Sections(Pages)

---
+++ pages/about/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about-page">
+    <h1>About US</h1>
+    <p>Morbi sodales rutrum tellus ut vehicula. Morbi vitae molestie sapien.
+      Fusce aliquet eget tortor vel convallis. Etiam sed porta urna.
+      Vestibulum eu neque quis sapien volutpat consectetur. Pellentesque semper vitae nisi vel
+      vehicula. Phasellus euismod, sapien ut faucibus vulputate, nunc sapien pretium diam,
+      sed placerat massa velit dictum ipsum. Phasellus in malesuada felis. Ut convallis lacinia
+      ligula vel luctus. Nam molestie interdum nisl ut vestibulum. Phasellus fermentum elementum
+      fringilla. Pellentesque dapibus erat sit amet urna mollis viverra. Maecenas maximus eget
+      magna non ultrices. Integer convallis dolor nunc, eget egestas turpis porttitor sed.
+    </p>
+    <p>Fusce vel sem efficitur, lobortis risus eget, malesuada orci. Praesent urna nisl,
+      efficitur non sodales nec, cursus non libero. Aenean vel erat leo. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Vivamus libero odio, luctus sit amet dolor sed,
+      vulputate ultrices eros. Donec tristique condimentum lacinia. In lacus sapien, sollicitudin
+      lobortis sapien eu, tristique egestas quam. Duis eget nulla nec nulla dapibus varius ac
+      placerat quam. Fusce ultricies venenatis ex, in egestas magna viverra eu. Nullam porttitor
+      ante ut feugiat posuere. Aenean lobortis mollis tempor. Maecenas aliquet tortor leo.
+      Praesent nec maximus arcu, eget fringilla tellus. Praesent ullamcorper vitae velit euismod
+      porttitor.
+    </p>
+    <p>
+      Etiam dui odio, tincidunt a lobortis pellentesque, porta non ligula. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit. Ut sit amet ipsum vel nunc elementum pellentesque.
+      Aenean bibendum nisl sit amet magna tincidunt semper. Aenean eu facilisis nulla.
+      Donec rutrum interdum tellus at auctor. In eu augue a ligula bibendum malesuada.
+      In sit amet eros augue. Nam facilisis faucibus imperdiet. Mauris id tristique risus,
+      id cursus odio. Phasellus facilisis quis enim quis sagittis. Orci varius natoque
+      penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vulputate nisl
+      quis volutpat porttitor.
+    </p>
+    Sed laoreet urna lacus, a tincidunt urna condimentum non. Nulla justo magna, gravida in orci vitae, imperdiet sollicitudin purus. Ut non purus quam. Quisque consectetur elit pellentesque feugiat tempor. Suspendisse vitae justo gravida, viverra tellus a, accumsan eros. Nam facilisis sapien quis metus scelerisque, eu mollis arcu laoreet. Nulla nisl metus, dapibus sit amet lorem sed, consequat bibendum ex. Duis sed lectus magna. Fusce luctus risus malesuada condimentum consectetur. Nam quis ornare libero, a molestie tellus. Cras volutpat dui quis mi placerat hendrerit.
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
