abe7d002c25277e01056e26fe29f56f260b95510
Aleksey Belchenkov <u608110@gmail.com>
2020-04-06 13:17:55 +0300

    Adding External Fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,8 @@
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+html, body {
+  font-family: 'Clicker Script', cursive;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
